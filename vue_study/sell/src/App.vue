155286727e4b5931c06a4cfdb771ef299500fabe
Everglow1 <1530305055.qq.com>
2018-03-09 15:11:52 +0800

    vue-router配置

--- vue_study/sell/src/App.vue
+++ vue_study/sell/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商品</div>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+         <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+         <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="content">
-      I am content
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -22,6 +26,22 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="stylus">
+  @import './common/stylus/mixin.styl'
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
+      .tab-item
+        flex: 1
+        text-align: center
+        & > a
+          display: block
+          text-decoration: none
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.active
+            color: rgb(240,20,20)
 </style>
