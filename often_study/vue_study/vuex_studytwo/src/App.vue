ded9a7c07491658b48b7a206113f33336aaa81c3
Everglow1 <1530305055.qq.com>
2018-04-01 12:32:04 +0800

    完成购物车结构搭建

---
+++ often_study/vue_study/vuex_studytwo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <productlist></productlist>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import productlist from './components/ProductList'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    productlist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
