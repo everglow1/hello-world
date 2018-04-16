8f1e10d825a0c1d933fbe1ce1039a6b17cecf315
Everglow1 <1530305055.qq.com>
2018-04-16 20:30:28 +0800

    加减符号组件引入

---
+++ vue_study/meituan-app/src/components/cartcontrol/CartControl.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cartcontrol">
+    <div class="cart-decrease icon-remove_circle_outline"></div>
+    <div class="cart-count">7</div>
+    <div class="cart-add icon-add_circle">
+      <i class="bg"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
