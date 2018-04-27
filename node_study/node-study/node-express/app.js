d2339c3b03d93b0815d3d12477d7e0573c7a1a6a
Everglow1 <1530305055.qq.com>
2018-04-27 16:28:33 +0800

    ejs模板引擎

--- node_study/node-study/node-express/app.js
+++ node_study/node-study/node-express/app.js
@@ -4,6 +4,9 @@ var express = require('express')
 // 实例化express对象
 var app = express()
 
+// 配置视图引擎 view engine 视图引擎
+app.set('view engine', 'ejs')
+
 // 通过对象调用对应的方法
 
 // 根据用户请求的地址，返回对应的数据信息
@@ -11,10 +14,11 @@ app.get('/',function (req, ress) {
   // 可以拿到用户请求的地址
   console.log(req.url)
   // 返回过来的信息
-  ress.send('this is home page!')
+  // ress.send('this is home page!')
+  ress.sendFile(__dirname + '/index.html')
 })
 app.get('/contact', function (req, res) {
-  res.send('this is contact')
+  res.sendFile(__dirname + '/contact.html')
   console.log(req.url)
 })
 // 通过get，可以对所有页面进行操作，假如有10个页面，写10个get就可以了
@@ -22,7 +26,9 @@ app.get('/contact', function (req, res) {
 // 路由参数
 // 当我们访问的路径为localhost:3000/(后面不管是啥)，都将返回（你所输入的路径参数为：它所输入的内容）
 app.get('/profile/:id', function (req, res) {
-  res.send('你访问的路径参数为：' + req.params.id)
+  // res.send('你访问的路径参数为：' + req.params.id)
+  // 展示profile.ejs
+  res.render('profile', {name: '我的'})
 })
 // 监听服务器端口号
 app.listen(8000)
