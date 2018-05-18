44142b6809fcf3f6de71bcf6ba6198649165bd4f
Everglow1 <1530305055.qq.com>
2018-05-18 18:47:15 +0800

    Module加载

--- often_study/js_study/test/index.js
+++ often_study/js_study/test/index.js
@@ -1182,3 +1182,39 @@ if (condition) {
 import( f() ).then();
 
 
+// Module的加载
+
+// 默认情况下，浏览器是同步加载 JavaScript 脚本，即渲染引擎遇到<script>标签
+// 就会停下来，等到执行完脚本，再继续向下渲染。如果是外部脚本，还必须加入脚本下载的时间。
+
+
+// 如果脚本体积很大，下载和执行的时间就会很长，因此造成浏览器堵塞，用户会感觉到浏览器“卡死”了，没有任何响应。
+// 这显然是很不好的体验，所以浏览器允许脚本异步加载，下面就是两种异步加载的语法。
+
+// 两种异步加载脚本的方法
+// {/* <script src="path/to/myModule.js" defer></script> */}
+// {/* <script src="path/to/myModule.js" async></script> */}
+// 标签打开defer或async属性，脚本就会异步加载。渲染引擎遇到这一行命令，
+// 就会开始下载外部脚本，但不会等它下载和执行，而是直接执行后面的命令。
+
+// defer与async的区别是：defer要等到整个页面在内存中正常渲染结束（DOM 结构完全生成，以及其他脚本执行完成），
+// 才会执行；async一旦下载完，渲染引擎就会中断渲染，执行这个脚本以后，再继续渲染。一句话，defer是“渲染完再执行”
+// async是“下载完就执行”。另外，如果有多个defer脚本，会按照它们在页面出现的顺序加载，而多个async脚本是不能保证加载顺序的。
+
+// 加载模块
+// 浏览器加载 ES6 模块，也使用<script>标签，但是要加入type="module"属性。
+// {/* <script type="module" src="./foo.js"></script> */}
+// 浏览器对于带有type="module"的<script>，都是异步加载，不会造成堵塞浏览器，即等到整个页面渲染完，再执行模块脚本，
+// 等同于打开了<script>标签的defer属性。
+
+// {/* <script type="module" src="./foo.js"></script>
+// // 等同于 
+// <script type="module" src="./foo.js" defer></script> */}
+
+// ES6模块与CommonJS模块的差异
+
+// 它们有两个重大差异。
+
+// CommonJS 模块输出的是一个值的拷贝，ES6 模块输出的是值的引用。
+// CommonJS 模块是运行时加载，ES6 模块是编译时输出接口。
+
