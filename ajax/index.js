3e14cdda89ea1a32a1f53114ba0e700ed1be5c6d
Everglow1 <1530305055.qq.com>
2018-05-28 19:50:58 +0800

    xhr对象概念

--- ajax/index.js
+++ ajax/index.js
@@ -7,3 +7,23 @@ if (window.XMLHttpRequest) {
 } else {
   xhr = new ActiveXObject('Microsoft.XMLHTTP'); // IE5 IE6
 }
+// 发送get请求
+xhr.open('GET', 'get.php', true);
+xhr.send();
+
+// 发送post请求
+xhr.open('POST', 'create.php', true);
+// 一定要写在open和send之间，不然会出错。
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.send('name=dog&sex=男');
+
+
+
+var request = new XMLHttpRequest();
+request.open('get', 'get.php', true);
+request.send();
+request.onreadystatechange = function () {
+  if (request.readyState === 4 && request.status === 200) {
+    // do something
+  }
+}
